83809cb7e9de04c4d123e6dad0c057ec0bd9f1cc
Stanislav Ageev <stanislav@agitek.io>
2024-07-07 19:46:50 +0500

    Dispatch to cloud tasks instead of direct http requests

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import fetch from 'node-fetch';
+import { CloudTasksClient } from '@google-cloud/tasks';
+
+const client = new CloudTasksClient();
 
 const app = express();
 const port = 8080;
+
+const project = 'enso-collective';
+const queue = 'lensPost';
+const location = 'us-central1';
+const url = 'https://us-central1-enso-collective.cloudfunctions.net/lensWebhook';
+const parent = client.queuePath(project, location, queue);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -33,14 +42,27 @@ app.post('/lens/notifications', async (req, res) => {
         profileId: message.data.pubProfileIdPointed
       };
 
-      await fetch('https://us-central1-enso-collective.cloudfunctions.net/lensWebhook', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
+      const task = {
+        httpRequest: {
+          headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          },
+          httpMethod: 'POST',
+          body: Buffer.from(JSON.stringify(postData)).toString("base64"),
+          url,
         },
-        body: JSON.stringify(postData)
-      });
+        scheduleTime: {
+          seconds: 0 + Date.now() / 1000
+        }
+      };
+
+      const request = { parent: parent, task: task };
+      try {
+        await client.createTask(request);
+      } catch(e) {
+        console.log('Failed to create a task: ' + e);
+      }
 
       res.sendStatus(200);
       return;
